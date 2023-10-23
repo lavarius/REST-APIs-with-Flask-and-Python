b32936b00213ee6a791786f0259a60c4ef6c315a
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2023-10-23 13:46:31 -0700

    Added del and update items

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def create_item():
             400,
             message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
         )
-    for item in item.values(): # duplicate validation
+    for item in items.values(): # duplicate validation
         if (
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
@@ -53,15 +53,10 @@ def create_item():
         abort(404, message= "Store not found.")
     
     item_id = uuid.uuid4().hex
-    item = {**item_id, "id": item_id}
+    item = {**item_data, "id": item_id}
     items[item_id] = item
     
     return item, 201
-
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
 
 
 @app.get("/store/<string:store_id>")
@@ -78,3 +73,38 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         abort(404, message= "Item not found.")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    # There's  more validation to do here!
+    # Like making sure price is a number, and also both items are optional
+    # You should also prevent keys that aren't 'price' or 'name' to be passed
+    # Difficult to do with an if statement...
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
