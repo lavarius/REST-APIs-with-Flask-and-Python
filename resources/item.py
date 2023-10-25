29a742ed678e4749dc1682c8d170842d8f116fdd
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2023-10-24 19:39:07 -0700

    Imports with SQLAlchemy methods in use

--- resources/item.py
+++ resources/item.py
@@ -2,9 +2,12 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
-# from db import items
+from db import db
 
 blp = Blueprint("Items", "items", description="Operations on items")
 
@@ -47,15 +50,12 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
+        item = ItemModel(**item_data)
 
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
 
         return item
