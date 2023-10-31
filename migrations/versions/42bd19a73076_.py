5c74d3899f7ab6588af537b78d075bbd0be526ed
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2023-10-31 15:01:04 -0700

    feature(upgrade): database upgrade on run

---
+++ migrations/versions/42bd19a73076_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 42bd19a73076
+Revises: e95b69d380ff
+Create Date: 2023-10-31 14:45:46.393848
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '42bd19a73076'
+down_revision = 'e95b69d380ff'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
