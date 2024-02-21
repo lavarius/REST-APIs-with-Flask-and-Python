d5b9c15941f544fa3811a31a10b3da8e0063484b
Mark Bartolo <hello@mark-bartolo.com>
2024-02-21 15:49:04 -0800

    Retrieve username from access_token

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,23 +1,24 @@
-from flask import url_for, redirect
+from flask import g
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from oa import github
 
-blp = Blueprint('github_login', __name__, url_prefix='/github')
+blp = Blueprint('github_login', __name__, url_prefix='/login')
 
-@blp.route("/login")
-def login():
-    redirect_uri = "http://localhost:5000/login/github/authorized"
-    return github.authorize_redirect(redirect_uri)
+@blp.route("/github")
+class GitHubLogin(MethodView):
+    @classmethod
+    def get(cls):
+        redirect_uri = "http://localhost:5000/login/github/authorized"
+        return github.authorize_redirect(redirect_uri)
 
 @blp.route('/github/authorized')
-def authorize():
-    token = github.authorize_access_token()
-    resp = github.get('user')
-    user_info = resp.json()
-    # Process the user_info and token as needed
-    return 'Logged in successfully with GitHub'
-# class GitHubLogin(MethodView):
-#     @classmethod
-#     def get(cls):
-#         return github.authorize_redirect(callback="http://localhost:5000/login/github/authorized")
+class GitHubAuthorize(MethodView):
+    @classmethod
+    def get(cls):
+        # Use authorize_access_token() to fetch the access token
+        github.authorize_access_token()
+        # g.access_token = resp.get('access_token')
+        github_user = github.get('user')
+        github_username = github_user.json().get('login')  # Adjusted this line from .data['login']
+        return github_username
